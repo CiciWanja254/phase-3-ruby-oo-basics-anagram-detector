1d129fba86ab9084d5a09275d48d88f8768d2e7a
Cici254 <thukucindy00@gmail.com>
2023-05-18 22:33:11 +0300

    Complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word.downcase
+    end
+  
+    def match(candidates)
+      candidates.select { |candidate| anagram?(candidate) }
+    end
+  
+    private
+  
+    def anagram?(candidate)
+      candidate.downcase != word && candidate.downcase.chars.sort == word.chars.sort
+    end
+  end
+  
